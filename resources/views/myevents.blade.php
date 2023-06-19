3345d6ce9fb35c4030bcd1484b04c9be0f1ff426
dsicmb <CB009983@students.apiit.lk>
2023-06-20 00:53:45 +0530

    my events blade modified
    my events blade modified

--- resources/views/myevents.blade.php
+++ resources/views/myevents.blade.php
@@ -7,19 +7,23 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="container mx-auto">
-                    <div class = "grid grid-cols-4 gap-4">
-                        @forelse ($events as $event)
-                            @livewire('event-card',[
-                            'event' => $event,
-                            ])
-                        @empty
-                            <p>
-                                No events registered at the moment
+            <div class="container mx-auto">
+                <div class = "grid grid-cols-4 gap-4">
+                    @forelse ($events as $event)
+                        @livewire('event-card',[
+                        'event' => $event,
+                        ])
+                    @empty
+                        <div class="flex flex-col justify-center items-center mt-10">
+                            <img src="/logo/noEvents.png" width="250px" alt="No Events" class="my-4 max-w-half">
+                            <p class="text-center text-m mt-4">
+                                Sorry, you did not reserve any events ...
                             </p>
-                        @endforelse
-                    </div>
+                        </div>
+                    @endforelse
+
                 </div>
-           </div>
-      </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
